a447d203cdeb19ea413ed2df9d86350e1ef66d07
Sage Gerard <sage@sagegerard.com>
2020-08-30 15:55:16 -0400

    Remove zcpkg.rkt

--- zcpkg.rkt
+++
@@ -1,7 +0,0 @@
-#lang info
-
-(define provider "sagegerard.com")
-(define package "file-watchers")
-(define description "Convenience functions for monitoring a file system")
-(define edition "draft")
-(define revision-number 0)
